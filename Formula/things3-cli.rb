392c0e6ff2b1eed264356a87cbce7a30e6efc25c
Ossian Hempel <hemposse@hotmail.com>
2026-01-02 10:48:26 +0100

    Update things3-cli formula

--- Formula/things3-cli.rb
+++ Formula/things3-cli.rb
@@ -1,14 +1,14 @@
 class Things3Cli < Formula
   desc "CLI for Things 3"
   homepage "https://github.com/ossianhempel/things3-cli"
-  url "https://github.com/ossianhempel/things3-cli/archive/959aeea72c81cf93a91c9811dd7c127e9b21b7d8.tar.gz"
-  sha256 "a994a73e258b97a4e941c636613c295a0a36061f7e7e205cf7930777e58aadad"
-  version "20260102103314"
+  url "https://github.com/ossianhempel/things3-cli/archive/7a0a4447170e7a54ece9b33fce55d1dc6d696726.tar.gz"
+  sha256 "9d503c8c8112d933946e3364ac6cb3ee90b9c86cc6bdd9644ee8fe64853efff2"
+  version "20260102104112"
 
   depends_on "go" => :build
 
   def install
-    ld_version = "959aeea"
+    ld_version = "7a0a444"
     ldflags = "-s -w -X github.com/ossianhempel/things3-cli/internal/cli.Version=#{ld_version}"
     system "go", "build", "-trimpath", "-ldflags", ldflags, "-o", bin/"things", "./cmd/things"
   end
