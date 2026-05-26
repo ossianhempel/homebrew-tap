ff73d05e243a7c1e912ea37ac3516ace983c900b
Ossian Hempel <hemposse@hotmail.com>
2026-05-26 21:58:23 +0200

    things3-cli v0.3.0

--- Formula/things3-cli.rb
+++ Formula/things3-cli.rb
@@ -1,15 +1,15 @@
 class Things3Cli < Formula
   desc "CLI for Things 3"
   homepage "https://github.com/ossianhempel/things3-cli"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.2.0/things-0.2.0-darwin-arm64.tar.gz"
-      sha256 "e6b84fee29e83cb39dc4b6a72480a6de05fc5876723dcc1f0a8e4e7c5f7f783f"
+      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.3.0/things-0.3.0-darwin-arm64.tar.gz"
+      sha256 "a6464403832457a935d7b7131d358802d5b751f4c7d25a06725b7bd3dad4e68f"
     else
-      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.2.0/things-0.2.0-darwin-amd64.tar.gz"
-      sha256 "ce9e1dadf7198c5c2f51d5eaf94f925110cf0170565e4066624802c2b104ca72"
+      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.3.0/things-0.3.0-darwin-amd64.tar.gz"
+      sha256 "235dc9f0a60499d392457114a52b83d5643045ee1020d9f3fa0a97444c6a4197"
     end
   end
 
