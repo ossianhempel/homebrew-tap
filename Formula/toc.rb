5b917a5dd79f6c889158a8ae60a424d07567ab42
Ossian Hempel <hemposse@hotmail.com>
2026-07-24 21:04:12 +0200

    feat: add toc CLI formula

---
+++ Formula/toc.rb
@@ -0,0 +1,16 @@
+class Toc < Formula
+  desc "Agent-native CLI and MCP server for Top of Class"
+  homepage "https://github.com/ossianhempel/top-of-class"
+  url "https://github.com/ossianhempel/top-of-class/releases/download/toc-v0.1.0/toc-0.1.0-macos-universal.zip"
+  sha256 "5c55173fd42b3d403d46efe65e3c782b88ee89b8cd3656e2f3ed958c448e707d"
+  version "0.1.0"
+
+  def install
+    bin.install "toc-0.1.0/bin/toc"
+  end
+
+  test do
+    assert_match "toc 0.1.0", shell_output("#{bin}/toc --version")
+    system bin/"toc", "mcp", "--help"
+  end
+end
